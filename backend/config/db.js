8b0d05a357ced119380ba2381662fd5cbd40101f
Anurag Chaurasiya <anurag9120959628@gmail.com>
2025-12-13 10:52:05 +0530

    add db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,18 +2,34 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
+export const db = mysql.createPool(process.env.DATABASE_URL);
 
-const isProd = process.env.NODE_ENV === "production";
 
-export const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  ssl: isProd
-    ? {
-        rejectUnauthorized: false, // RAILWAY requires this
-      }
-    : false,
-});
+
+
+
+
+
+
+
+
+
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// export const db = await mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   ssl: isProd
+//     ? {
+//         rejectUnauthorized: false, // RAILWAY requires this
+//       }
+//     : false,
+// });
